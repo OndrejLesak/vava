9572eaa643a4c9e8d2a8194545c537bd7a16bc35
Adrián Hládek <71824498+Adrian-Hladek@users.noreply.github.com>
2023-04-25 16:47:00 +0200

    upravene add recept

--- CookingManager/src/main/java/eu/fiit/cookingmanager/cookingmanager/controller/AddRecipeController.java
+++ CookingManager/src/main/java/eu/fiit/cookingmanager/cookingmanager/controller/AddRecipeController.java
@@ -1,14 +1,19 @@
 package eu.fiit.cookingmanager.cookingmanager.controller;
 
 
+
 import eu.fiit.cookingmanager.cookingmanager.utils.DBUtils;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.w3c.dom.Document;
@@ -19,24 +24,147 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.Arrays;
 
 public class AddRecipeController implements Initializable {
 
     @FXML private Button btn_xmlFile;
     @FXML private Button btn_logout;
     @FXML private Button btn_back_home;
+
+    @FXML private Button btn_adding;
     @FXML private Label lbl_xmlFile;
 
     @FXML
+    private TextField inputName;
+    @FXML
+    private TextField inputTime;
+    @FXML
+    private TextArea inputSteps;
+    @FXML
     private Button btn_recipe;
+
+    @FXML
+    private VBox vbox_ingredients;
+    @FXML private ChoiceBox choiceType;
     @FXML private Button btn_discard;
     FileChooser fileChooser = new FileChooser();
     File selectedFile = null;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+
+
+
+
+
+
+        vbox_ingredients.setSpacing(10);
+        vbox_ingredients.setPadding(new Insets(10));
+
+        Object[][] arr_ing = {};
+        try{//Get information about all ingredients and fill into array
+            Connection conn = new DBUtils().dbConnect();
+            String query = "Select * from public.ingredient";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            ResultSet rs = pstmt.executeQuery();
+
+
+
+
+            while (rs.next()) {
+                //System.out.println("while");
+                int ing_id = rs.getInt("id");
+
+                String ing_name = rs.getString("name");
+
+                String ing_uom = rs.getString("uom");
+
+                int ing_weight = 0;
+                //String ing_weight = rs.getString("type"); weight tam nieje v DB
+
+
+                Object arrNew[][] = new Object[arr_ing.length + 1][3];
+
+
+                for (int i = 0; i < arr_ing.length; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        arrNew[i][j] = arr_ing[i][j];
+                    }
+                }
+
+
+                arrNew[arrNew.length -1] = new Object[] {ing_id, ing_name, ing_uom,ing_weight};
+                arr_ing = arrNew.clone();
+
+                //System.out.println(arr_ing[0][1]);
+                //System.out.println(arr_ing[0]);
+            }
+            DBUtils.dbDisconnect(conn);
+
+
+            //System.out.println(arr_ing[1]);
+            choiceType.setItems(FXCollections.observableArrayList(arr_ing)
+            );
+            choiceType.setValue("");
+        }
+        catch (Exception e) {
+
+            lbl_xmlFile.setText("Database disconnected");
+            lbl_xmlFile.setTextFill(Color.color(1,0,0));
+            e.printStackTrace();
+
+        }
+
+
+
+
+
+
+
+        String [] arr = {};
+        try{//Get information about all food types and fill the panel
+        Connection conn = new DBUtils().dbConnect();
+        String query = "Select type from public.food_type";
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        ResultSet rs = pstmt.executeQuery();
+
+
+        //robiš že hladáš types s DB
+            while (rs.next()) {
+
+                String food_type_element = rs.getString("type");
+
+                String arrNew[] = new String[arr.length + 1];
+                int i;
+                for(i = 0; i < arr.length; i++) {
+                    arrNew[i] = arr[i];
+                }
+                arrNew[i] = food_type_element;
+                arr = arrNew.clone();
+            }
+            DBUtils.dbDisconnect(conn);
+
+
+            System.out.println(arr[1]);
+        choiceType.setItems(FXCollections.observableArrayList(arr)
+        );
+        choiceType.setValue("");
+        }
+         catch (Exception e) {
+
+             lbl_xmlFile.setText("Database disconnected");
+             lbl_xmlFile.setTextFill(Color.color(1,0,0));
+             e.printStackTrace();
+
+        }
 
 
         btn_xmlFile.setOnAction(new EventHandler<ActionEvent>(){
@@ -48,6 +176,10 @@ public class AddRecipeController implements Initializable {
                 if(selectedFile != null){
 
                     System.out.println(selectedFile.getAbsolutePath());
+                    inputName.setText("");
+                    inputTime.setText("");
+                    inputSteps.setText("");
+                    choiceType.setValue("");
 
                     try {
                         // Create a new DocumentBuilderFactory and DocumentBuilder
@@ -69,8 +201,8 @@ public class AddRecipeController implements Initializable {
                                     Element receptElement = (Element) receptNode;
                                     String name = receptElement.getElementsByTagName("name").item(0).getTextContent();
                                     String type = receptElement.getElementsByTagName("type").item(0).getTextContent();
-                                    int time = Integer.parseInt(receptElement.getElementsByTagName("time").item(0).getTextContent());
-                                    NodeList ingredientList = receptElement.getElementsByTagName("ingredient");
+                                    String time = receptElement.getElementsByTagName("time").item(0).getTextContent();
+                                     NodeList ingredientList = receptElement.getElementsByTagName("ingredient");
                                     String steps = receptElement.getElementsByTagName("steps").item(0).getTextContent();
 
                                     System.out.println("Name: " + name);
@@ -90,17 +222,50 @@ public class AddRecipeController implements Initializable {
                                     System.out.println();
 
 
+
+
+
+                                    Connection conn = new DBUtils().dbConnect();
+                                    String query = "Select * from public.food_type where type=(?)";
+
+                                    PreparedStatement pstmt = conn.prepareStatement(query);
+
+                                    pstmt.setString(1, type);
+
+                                    ResultSet rs = pstmt.executeQuery();
+
+                                    boolean is_food_type = rs.next();
+                                    DBUtils.dbDisconnect(conn);
+
+                                    if( is_food_type){
+                                        System.out.println("Hello this food is here");
+                                        inputName.setText(name);
+                                        inputTime.setText(time);
+                                        inputSteps.setText(steps);
+                                        choiceType.setValue(type);
+                                        lbl_xmlFile.setText(selectedFile.getName());
+                                        lbl_xmlFile.setTextFill(Color.color(0,0,0));
+                                     }
+                                    else
+                                    {
+                                        lbl_xmlFile.setText("Invalid food type");
+                                        lbl_xmlFile.setTextFill(Color.color(1,0,0));
+
+                                    }
+
                                 }
                             }
-                            lbl_xmlFile.setText(selectedFile.getName());
+
                         }
                         else {
                             lbl_xmlFile.setText("Invalid XML file");
+                            lbl_xmlFile.setTextFill(Color.color(1,0,0));
                         }
 
 
                     } catch (Exception e) {
                         lbl_xmlFile.setText("Invalid XML file");
+                        lbl_xmlFile.setTextFill(Color.color(1,0,0));
                         e.printStackTrace();
 
                     }
@@ -146,7 +311,44 @@ public class AddRecipeController implements Initializable {
                 DBUtils.changeScene(actionEvent, "home.fxml", "Cooking Manager", resourceBundle);
             }
         });
+
+        btn_adding.setOnAction(event -> addNewChoiceBox());
+
+
+        /*btn_adding.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+                //DBUtils.changeScene(actionEvent, "login.fxml", resourceBundle.getString("login_title"), resourceBundle);
+                System.out.println("adding");
+            }
+        });
+        */
+
+
+
     }
+
+    private void addNewChoiceBox() {
+
+        HBox hbox = new HBox();
+        hbox.setSpacing(10);
+
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        choiceBox.getItems().addAll("Flour", "Sugar", "Butter", "Salt");
+
+        // Create a delete button for the new ChoiceBox
+        Button deleteButton = new Button("Delete");
+        deleteButton.setOnAction(event -> {
+            vbox_ingredients.getChildren().remove(hbox);
+        });
+
+        hbox.getChildren().addAll(choiceBox, deleteButton);
+
+        // Add the HBox to the VBox layout
+        vbox_ingredients.getChildren().add(hbox);
+    }
+
 
 
 
